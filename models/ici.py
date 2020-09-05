1cb0f6bbdab33bfdffdaa7bc16b25fcc0de052ad
Song <quanpengsong@gmail.com>
2020-09-05 22:35:41 +0800

    save work

--- models/ici.py
+++ models/ici.py
@@ -1,17 +1,31 @@
 import math
-
+import time
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import ElasticNet
 from sklearn.preprocessing import normalize
 
 
+def plot_regularization_path(alpha, coefs, query_y, pseudo_y):
+    coefs = np.sum(np.abs(coefs.transpose(2, 1, 0)[::-1, :, :]), axis=2)  # 100, 80
+    alpha = np.log10(alpha[::-1])  # 100
+    plt.clf()
+    plt.figure(1)
+    for i in range(5):
+        plt.plot(alpha, coefs[:, i], c="y")
+    for i in range(5, 80):
+        t = i - 5
+        c = "r" if query_y[t] == pseudo_y[t] else "black"
+        plt.plot(alpha, coefs[:, i], c=c, linestyle="-")
+    plt.savefig(f"./temp/path_{time.time()}.png")
+
+
 class ICI(object):
 
-    def __init__(self, classifier='lr', num_class=None, step=5, max_iter='auto',
-                 reduce='pca', d=5, norm='l2'):
-        self.step = step
+    def __init__(self, classifier='lr', num_class=None, step=5, max_iter='auto', reduce='pca', d=5, norm='l2'):
+        self.step = step  # the number of unlabeled data selected for each class in one iteration
         self.max_iter = max_iter
-        self.num_class = num_class
+        self.num_class = num_class  # the number of classes per task
         self.initial_embed(reduce, d)
         self.initial_norm(norm)
         self.initial_classifier(classifier)
@@ -23,6 +37,13 @@ class ICI(object):
         self.support_y = y
 
     def predict(self, X, unlabeled_X=None, show_detail=False, query_y=None):
+        """
+        1. Train a liner classifier with support set.
+        2. Use the classifier to get pseudo label for unlabeled data.
+        3. Choose a subset from the unlabeled data which has high credibility inferred by the ICI algorithm
+           and expand this subset to the support set.
+        4. Go to step 1 until converged or maximum number of iterations reached.
+        """
         support_X, support_y = self.support_X, self.support_y
         way, num_support = self.num_class, len(support_X)
         query_X = self.norm(X)
@@ -44,17 +65,20 @@ class ICI(object):
         else:
             assert float(self.max_iter).is_integer()
         support_set = np.arange(num_support).tolist()
+        # Train classifier using (Xs; ys);
         self.classifier.fit(self.support_X, self.support_y)
         acc_list = []
         for _ in range(self.max_iter):
             if show_detail:
                 predicts = self.classifier.predict(query_X)
                 acc_list.append(np.mean(predicts == query_y))
+            # Get pseudo-label yu for Xu by classifier
             pseudo_y = self.classifier.predict(unlabeled_X)
             y = np.concatenate([support_y, pseudo_y])
             Y = self.label2onehot(y, way)
             y_hat = np.dot(X_hat, Y)
-            support_set = self.expand(support_set, X_hat, y_hat, way, num_support, pseudo_y, embeddings, y)
+            # Use the ICI algorithm to expand our support set.
+            support_set = self.expand(support_set, X_hat, y_hat, way, num_support, pseudo_y, embeddings, y, query_y)
             y = np.argmax(Y, axis=1)
             self.classifier.fit(embeddings[support_set], y[support_set])
             if len(support_set) == len(embeddings):
@@ -65,18 +89,28 @@ class ICI(object):
             return acc_list
         return predicts
 
-    def expand(self, support_set, X_hat, y_hat, way, num_support, pseudo_y, embeddings, targets):
-        _, coefs, _ = self.elastic_net.path(X_hat, y_hat, l1_ratio=1.0)
+    def expand(self, support_set, X_hat, y_hat, way, num_support, pseudo_y, embeddings, targets, query_y):
+        """
+        Here is the key of the ICI algorithm.
+        """
+        # Check: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.ElasticNet.html#sklearn.linear_model.ElasticNet.path
+        # Coefficients along the path with shape: (n_features, n_alphas)
+        alphas, coefs, _ = self.elastic_net.path(X_hat, y_hat, l1_ratio=1.0)
+        # self.plot_regularization_path(alphas, coefs, pseudo_y, query_y)
         coefs = np.sum(np.abs(coefs.transpose(2, 1, 0)[::-1, num_support:, :]), axis=2)
         selected = np.zeros(way)
         for gamma in coefs:
+            # Rank (X; y) = (X; [ys; yu]) by ICI;
             for i, g in enumerate(gamma):
+                # Select a subset (X_sub; y_sub) into (Xs; ys);
                 if g == 0.0 and (i + num_support not in support_set) and (selected[pseudo_y[i]] < self.step):
                     support_set.append(i + num_support)
                     selected[pseudo_y[i]] += 1
             if np.sum(selected >= self.step) == way:
                 break
         return support_set
+
+    # Copied from: https://github.com/Yikai-Wang/ICI-FSL/issues/4
 
     def initial_embed(self, reduce, d):
         reduce = reduce.lower()
